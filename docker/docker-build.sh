4b883045931dc8001c428b8e20eba22855a5696f
yaohwu <yaohwu@163.com>
2024-06-06 17:30:38 +0800

    single-merged and multi way to build different arch docker image

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+plan="$1"
+multi_plan="multi"
+single_merged_plan="single"
+plan=${plan-"$multi_plan"}
+
+echo "using plan [$plan] to build docker image, supported plans: [$multi_plan,$single_merged_plan]"
+
 set -eux
 
 fine_builder=${fine_builder-"fine_builder"}
@@ -17,9 +24,31 @@ set -e
 
 target_project=${target_project-"yaohwu"}
 
-# 使用 --platform 交叉构建出跨平台的多个镜像
-# 使用 -t 指定 tag，跨平台共享一个tag；会在 pull 时使用本机的架构 pull 到对应的镜像，如果没有匹配的，则会直接拋错
-# 使用 --push 直接推送，buildx 不会 export 出对应 tag 的多个镜像
-docker buildx build --builder $fine_builder --no-cache --platform linux/amd64,linux/arm64 -t "$target_project"/debian:bookworm-slim --push .
+echo "using $plan plan to build docker image docker build debian image end"
 
+if [ "$plan" == "$multi_plan" ]; then
+  # 使用 --platform 交叉构建出跨平台的多个镜像
+  # 使用 -t 指定 tag，跨平台共享一个tag；会在 pull 时使用本机的架构 pull 到对应的镜像，如果没有匹配的，则会直接拋错
+  # 使用 --push 直接推送，buildx 不会 export 出对应 tag 的多个镜像
+  docker buildx build --builder $fine_builder --platform linux/amd64,linux/arm64 -t "$target_project"/debian:bookworm-slim --push .
+
+elif
+  [ "$plan" == "$single_merged_plan" ]
+then
+  # amd64
+  docker buildx build --builder $fine_builder --platform linux/amd64 -t "$target_project"/debian:bookworm-slim-amd64 --load .
+  docker push "$target_project"/debian:bookworm-slim-amd64
+  # arm64
+  docker buildx build --builder $fine_builder --platform linux/arm64 -t "$target_project"/debian:bookworm-slim-arm64 --load .
+  docker push "$target_project"/debian:bookworm-slim-arm64
+
+  # 创建和维护清单文件
+  docker manifest create "$target_project"/debian:bookworm-slim \
+    --amend "$target_project"/debian:bookworm-slim-amd64 \
+    --amend "$target_project"/debian:bookworm-slim-arm64
+  echo "docker single manifest created"
+
+  # 推送 multi-architecture 镜像
+  docker manifest push "$target_project"/debian:bookworm-slim
+fi
 echo "docker build debian image end"
